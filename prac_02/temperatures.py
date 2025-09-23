e7b08ddfbeea9f9f02351539a8735e1b716ba7f6
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-23 14:38:16 +1000

    Add docstrings to functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -2,36 +2,38 @@
 CP1404/CP5632 - Practical
 Program for temperature conversion
 """
-
-
-def main():
-    menu = """C - Convert Celsius to Fahrenheit
+MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
 
-    print(menu)
+
+def main():
+   """Program for temperature conversion"""
+    print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
             fahrenheit = convert_celsius_to_fahrenheit()
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
-            celsius = convert_fahrenhiet_to_celsius()
+            celsius = convert_fahrenheit_to_celsius()
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
-        print(menu)
+        print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
 
 
-def convert_fahrenhiet_to_celsius() -> float:
+def convert_fahrenheit_to_celsius():
+    """Convert fahrenheit to celsius"""
     fahrenheit = float(input("Fahrenheit : "))
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
-def convert_celsius_to_fahrenheit() -> float:
+def convert_celsius_to_fahrenheit():
+    """Convert celsius to fahrenheit"""
     celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
