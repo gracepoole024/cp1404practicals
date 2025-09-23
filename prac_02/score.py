d25f25f2b6790d990d3c1dff76076a6bc01a767a
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-23 14:49:34 +1000

    Add docstrings to functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,23 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """Program to determine score satus"""
+    score = float(input("Enter score: "))
+    determine_grade(score)
+
+
+def determine_grade(score: float):
+    """Determine status of given score"""
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
