118c91b8ee8a816356be447c2ea67df4b2581f38
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-23 13:54:35 +1000

    Add docstring comments

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,20 +4,22 @@ MINIMUM_LENGTH = 5
 
 
 def main():
-    password = get_name()
+    password = get_password()
 
     while len(password) < MINIMUM_LENGTH:
         print(f"Password must be Longer that {MINIMUM_LENGTH} characters")
-        password = get_name()
+        password = get_password()
 
     print_star(password)
 
 
 def print_star(password: str):
+    """Print *'s the length of password"""
     print("*" * len(password))
 
 
-def get_name() -> str:
+def get_password() -> str:
+    """Get Password from user"""
     password = str(input("Set Password: "))
     return password
 
