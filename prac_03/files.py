ef5336f94f31aebaa16fd340363c7cbd04d566a4
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-30 16:02:51 +1000

    Complete question #3 and #4

--- prac_03/files.py
+++ prac_03/files.py
@@ -11,4 +11,15 @@ print(f"Hi {name}")
 in_file.close()
 
 # 3
+with open("numbers.txt", 'r') as in_file:
+    number_01 = int(in_file.readline())
+    number_02 = int(in_file.readline())
+print(number_01 + number_02)
 
+# 4
+lines = 0
+with open("numbers.txt", 'r') as in_file:
+    for line in in_file:
+        number = int(line)
+        lines += number
+print(lines)
