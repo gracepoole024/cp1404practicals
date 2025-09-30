2f95f0bc61403896e1463d810095fc26cc367c47
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-30 13:16:16 +1000

    Add file to open for writing to

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -16,26 +16,21 @@ MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 FILENAME = "readfile"
 
+out_file = open(FILENAME, 'w')
+
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 number_of_days = 0
 
-out_file = open(FILENAME, 'w')
-
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
         price_change = random.uniform(0, MAX_INCREASE)
     else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
     number_of_days += 1
-    print(f"On day {number_of_days} price is ${price:,.2f}")
+    print(f"On day {number_of_days} price is ${price:,.2f}", file=out_file)
+out_file.close()
