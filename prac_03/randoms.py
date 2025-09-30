0f80b21b82106dd8c2462125f4cb4bd7c3398089
jd115401 <grace.poole@my.jcu.edu.au>
2025-09-30 12:50:28 +1000

    Add random functions test

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+"""
+What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?
+The smallest number possible is 5, and the largest is 20.
+
+What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+The smallest number possible is 3, and the largest is 9. Line 2 could not produce 4 as it starts at 3 and goes up
+in 2's, so the range is 3, 5, 7, 9.
+
+What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?
+The smallest number possible is 2.5, and the largest is 5.5.
+
+"""
+
+
+import random
+
+print(random.randint(1, 100))
