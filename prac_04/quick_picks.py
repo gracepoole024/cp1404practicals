c31d1b2473ffde41ce26623f4c0f6dfa45e85255
jd115401 <grace.poole@my.jcu.edu.au>
2025-10-07 16:50:46 +1000

    Refactor prac 4 program to include functions

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,18 +9,27 @@ NUMBERS_ON_LINE = 6
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 
-number_of_quick_picks = int(input("How many quick picks? "))
-while number_of_quick_picks < 0:
-    print("Enter positive integer")
+
+def main():
+    """Quick picks program."""
     number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Enter positive integer")
+        number_of_quick_picks = int(input("How many quick picks? "))
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        randomise_numbers(quick_pick)
+        print(" ".join(f"{number:2}" for number in quick_pick))
 
-for i in range(number_of_quick_picks):
-    quick_pick = []
 
+def randomise_numbers(quick_pick):
+    """Choose set of random numbers according to CONSTANTS."""
     for j in range(NUMBERS_ON_LINE):
         number = randint(MIN_NUMBER, MAX_NUMBER)
         while number in quick_pick:
             number = randint(MIN_NUMBER, MAX_NUMBER)
         quick_pick.append(number)
     quick_pick.sort()
-    print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+main()
