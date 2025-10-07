8af3838038aff398a8ffe656812ee5de48d0d28d
jd115401 <grace.poole@my.jcu.edu.au>
2025-10-07 14:41:17 +1000

    Add security checker to list_exercises prac 4

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,13 +1,27 @@
 """
 CP1404/CP5632 Practical 4
-Basic list operations
+Basic list operations, and woefully inadequate security checker
 """
 
-numbers = [int(input("Number: ")), int(input("Number: ")), int(input("Number: ")), int(input("Number: ")),
-           int(input("Number: "))]
+# Basic list operations
+numbers = []
+for i in range(5):
+    numbers = int(input("Numbers: "))
+    numbers.append(numbers)
 
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter Username: ")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
