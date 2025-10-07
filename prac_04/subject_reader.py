6e0756ce070f0c87d3a1bf3a819ab3d6e3a1bd50
jd115401 <grace.poole@my.jcu.edu.au>
2025-10-07 13:28:30 +1000

    Add nested list to prac 4

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,16 +14,14 @@ def main():
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    subject_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_data.append(parts)
     input_file.close()
+
 
 
 main()
