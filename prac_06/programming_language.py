244fa34d9a195057bb4294bb32f9f43d9613c0a1
jd115401 <grace.poole@my.jcu.edu.au>
2025-10-28 13:43:39 +1000

    Add constructor method to initialise ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,16 @@
+"""CP1404/CP5632 Practical - Programming class."""
+
+"""
+Estimated: 20 minutes
+Actual: 
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage object."""
+
+    def __init__(self, typing="", reflection=False, year=0):
+        """Initialise a programming language instance."""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
