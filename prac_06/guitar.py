898fa4f9f3976d1ee9105b5b4ceb6fcf99e49473
jd115401 <grace.poole@my.jcu.edu.au>
2025-10-30 16:26:27 +1000

    Add docstring comments

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,14 +1,26 @@
+"""CP1404/CP5632 Practical - Guitar class."""
+
+CURRENT_YEAR = 2017
+VINTAGE_AGE = 50
+
+
 class Guitar:
+    """Represent a Guitar object."""
+
     def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
 
-    def __get_age__(self):
-        self.age = 2025 - self.year
+    def get_age(self):
+        """Get age in years of a guitar"""
+        return CURRENT_YEAR - self.year
 
-    def __is_vintage__(self):
-        return self.age >= 50
+    def is_vintage(self):
+        """Determine if a guitar is vintage"""
+        return self.get_age() >= VINTAGE_AGE
 
-    def __str__(self, age):
-        return f"{self.name} ({age}) : ${self.cost}"
+    def __str__(self):
+        """Return a string representation of a guitar."""
+        return f"{self.name} ({self.get_age()}) : ${self.cost}"
