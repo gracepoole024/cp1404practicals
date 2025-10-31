dd9f6c5b94d0251ed03d775370aa3a0705009f26
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-01 09:53:07 +1000

    Add main function to run guitars program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,21 +1,29 @@
 """CP1404/CP5632 Practical - Playing the Guitars."""
 from prac_06.guitar import Guitar
 
-print("My guitars!")
 
-guitars = []
+def main():
+    print("My guitars!")
 
-name = input("Name: ")
-while name != "":
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    guitars.append(Guitar(name, year, cost))
-    print(f"{name} ({year}) : ${cost: 5} added")
+    guitars = []
+
     name = input("Name: ")
-guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost: 5} added")
+        name = input("Name: ")
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-print("These are my guitars:")
-for i, guitar in enumerate(guitars, 1):
-    vintage = "(vintage)" if guitar.is_vintage() else ""
-    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage}")
+    if guitars:  # lists, strings and other collections are False when empty, True when non-empty
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage = "(vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage}")
+    else:
+        print("Go buy a guitar")
+
+
+main()
