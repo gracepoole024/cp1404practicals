ac710c49c60d3b31e6a72e62a288e7f17579a2ce
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-01 09:47:43 +1000

    Add ternary operator to determine if a guitar is vintage

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,10 +9,13 @@ name = input("Name: ")
 while name != "":
     year = int(input("Year: "))
     cost = float(input("Cost: $"))
-    guitar = Guitar(name, year, cost)
-    guitars.append(guitar)
-    print(f"{name} ({year}) : ${cost: 5}")
+    guitars.append(Guitar(name, year, cost))
+    print(f"{name} ({year}) : ${cost: 5} added")
     name = input("Name: ")
-print("These are my guitars: ")
-print(f"Guitar 1: {name} ({year}), worth ${cost}")
+guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
+print("These are my guitars:")
+for i, guitar in enumerate(guitars, 1):
+    vintage = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage}")
