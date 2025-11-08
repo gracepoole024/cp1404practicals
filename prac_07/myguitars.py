ca2f0dd4ebe2942343212ac197db9a6792ca7aea
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-08 11:38:35 +1000

    Display guitars using a for loop and enumerate

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,10 +7,14 @@ FILENAME = "guitars.csv"
 def main():
     guitars = []
     with open(FILENAME, "r") as in_file:
-        in_file.readline()  # skip heading
         for line in in_file:
             parts = line.strip().split(",")
-            guitars.append(Guitar(parts[0], parts[1], parts[2]))
-    print(guitars)
+            guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
+
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage = "(vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>30} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage}")
+
 
 main()
