72bf4ac3a11163209b47e0ba069852342630a6b3
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-09 10:22:07 +1000

    Add add_new_project function to add projects to list

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,7 @@ CP1404 Practical 7 - Project Management Program
 Estimated time:3hrs
 Actual time:
 """
+
 from prac_07.project import Project
 
 FILENAME = "projects.txt"
@@ -28,11 +29,11 @@ def main():
         elif choice == "S":
             pass
         elif choice == "D":
-            pass
+            display_projects(projects)
         elif choice == "F":
             pass
         elif choice == "A":
-            pass
+            projects = add_new_project(projects)
         else:
             print("Invalid menu choice")
         print(MENU)
@@ -42,7 +43,29 @@ def main():
 def read_projects_file(filename):
     projects = []
     with open(filename, "r") as in_file:
+        in_file.readline()
         for line in in_file:
-            in_file.readline()
-            parts = line.strip().split(",")
+            parts = line.strip().split("\t")  # Split by tab not '
             projects.append(Project(parts[0], (parts[1]), int(parts[2]), float(parts[3]), float(parts[4])))
+    return projects
+
+
+def display_projects(projects):
+    for project in projects:
+        print(
+            f"{project.name}, {project.start_date}, {project.priority}, {project.cost}, {project.completion_percentage}")
+
+
+def add_new_project(projects):
+    """Add a project by prompting user for project details."""
+    name = input("Project Name: ")
+    start_date = input("Start Date: ")
+    priority = int(input("Priority: "))
+    cost = float(input("Estimated Cost: "))
+    completion_percentage = float(input("Completion Percentage: "))
+    projects.append(Project(name, start_date, priority, cost, completion_percentage))
+    return projects
+
+
+if __name__ == '__main__':
+    main()
