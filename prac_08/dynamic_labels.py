bd8a35e37fe59e05006f4b9fb6101d2eef37dd73
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-13 16:15:50 +1000

    Add main program for app logic to create dynamic labels

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,32 @@
+"""CP1404 Practical 8 - Dynamic Labels"""
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.button import Button
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app for dynamic label widget creation."""
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        # basic data for list of names
+        self.names = ["John", "Paul", "George", "Ringo"]
+
+    def build(self):
+        """Build Kivy GUI"""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """Create dynamic labels from data and add them to GUI."""
+        for name in self.names:
+            label = Label(text=name)
+            self.root.ids.entries_box.add_widget(label)
+
+
+DynamicLabelsApp().run()
