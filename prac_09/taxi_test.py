5d1bc28bb5389469bb050692d3ba4487e139e76c
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-22 09:05:16 +1000

    Add test program to test Taxi SubClass

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,12 @@
+"""CP1404 Prac 9 - Test Taxi SubClass"""
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Program to text Taxi class."""
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
