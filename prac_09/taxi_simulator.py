9d9140dee66e7df2ab6295d2eb1fd1189cdffdba
jd115401 <grace.poole@my.jcu.edu.au>
2025-11-23 09:17:02 +1000

    Refactor display taxis into separate functon for SRP

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -4,22 +4,21 @@ from prac_06.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
-Menu = """q)uit, c)hoose taxi, d)rive"""
+MENU = """q)uit, c)hoose taxi, d)rive"""
 
 
 def main():
-    total_fare = 0
+    total_fare = 0.0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
     print("Let's drive!")
-    print(Menu)
+    print(MENU)
     choice = input(">>").lower()
     while choice != "q":
         if choice == "c":
             print("Taxis available:")
-            for i, taxi in enumerate(taxis):
-                print(f"{i} - {taxi}")
+            display_taxis(taxis)
         elif choice == "d":
             if current_taxi:
                 current_taxi.start_fare()
@@ -27,6 +26,16 @@ def main():
                 current_taxi.drive(distance)
                 cost = current_taxi.get_fare()
                 print(f"Your {current_taxi.name} trip cost you ${cost}")
+                total_fare += cost
             else:
                 print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid menu choice")
+        print(f"Bill to date: ${total_fare}")
+        print(MENU)
+        choice = input(">>").lower()
 
+
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
